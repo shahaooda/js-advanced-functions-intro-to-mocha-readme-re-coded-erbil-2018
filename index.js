4235ad6d55c2d525958f21451dd9c48ea563e26a
Shahad Bakoos <shahadbakoos@gmail.com>
2019-02-07 14:11:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth()
+{
+  return true;
+}
+function sayHey()
+{
+  return 'Hey!'
+}
+function sayHeyFriend(name)
+{
+  return `Hey, ${name}!`
+}
+function favoriteIceCream(name)
+{
+  return `I love ${name}`;
+}
+function shouting(str)
+{
+  return str.toUpperCase();
+}
+function roundDown(num)
+{
+  return Math.floor(num)
+}
